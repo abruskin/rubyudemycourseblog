a57a00b0caf5dbcc8feb0c4d2ffa891c29cac645
abruskin <90646708+abruskin@users.noreply.github.com>
2025-10-31 08:39:22 -0500

    adding author to article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User <ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    has_many :articles
+
+    validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }
+  
+    validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false },
+                      format: { with: VALID_EMAIL_REGEX }
+
 end
---
+++ db/migrate/20251030204601_add_relationship_between_user_id_and_article.rb
@@ -0,0 +1,5 @@
+class AddRelationshipBetweenUserIdAndArticle < ActiveRecord::Migration[8.0]
+  def change
+    add_column :articles, :user_id, :int
+  end
+end
