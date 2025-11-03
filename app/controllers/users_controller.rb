5a7c1194ffe176cf7b318a37e811580433959198
abruskin <90646708+abruskin@users.noreply.github.com>
2025-11-03 12:26:44 -0600

    adding restriction logic and admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def new
         @user = User.new
     end
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice]="Welcome to ALPHABLOG, #{@user.username}! You have successfully signed up"
             redirect_to articles_path
         else
@@ -15,11 +18,9 @@ class UsersController < ApplicationController
     end 
 
     def edit 
-        @user = User.find(params[:id])
     end 
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was succesfully updated"
             redirect_to @user
@@ -29,7 +30,6 @@ class UsersController < ApplicationController
     end 
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -37,8 +37,27 @@ class UsersController < ApplicationController
         @users = User.paginate(page: params[:page], per_page: 5)
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if current_user == @user
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to articles_path
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end 
+
+    def require_same_user
+         if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own profile"
+        redirect_to @user
+      end 
+    end
+
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User <ApplicationRecord
     before_save {self.email = email.downcase}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    has_many :articles
+    has_many :articles, dependent: :destroy
 
     validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }
